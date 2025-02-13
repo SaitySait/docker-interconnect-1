94dbd4961a42e7f39c979ac2b923d6db037d0857
dang-bowvalleycollege <123997086+dang-bowvalleycollege@users.noreply.github.com>
2025-02-13 00:46:25 +0000

    update code

---
+++ docker-interconnect/app3/app.py
@@ -0,0 +1,12 @@
+import requests
+from flask import Flask, jsonify
+
+app = Flask(__name__)
+
+@app.route('/call-app1', methods=['GET'])
+def call_app1():
+    response = requests.get("http://app1:5000/api/message")  # Use service name `app1`
+    return jsonify({"response_from_app1": response.json()})
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5002)
